2b44b28ca43ddc8382990ea4b0da7e60c23233a1
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-03-15 21:06:22 +0800

    Facebook Integration successful with customUI. Normal Login working too.

--- app/src/main/java/com/shantanukamath/knockout/MainActivity.java
+++ app/src/main/java/com/shantanukamath/knockout/MainActivity.java
@@ -47,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SurveyActivity.class);
+            startActivity(i);
             return true;
         }
         else if (id == R.id.logout)
