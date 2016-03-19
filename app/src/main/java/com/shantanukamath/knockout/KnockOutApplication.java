f898c061657bd1ba0c2b558ccecf4791aa7eff97
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-03-20 01:55:01 +0800

    Map working on its own activity :P

--- app/src/main/java/com/shantanukamath/knockout/KnockOutApplication.java
+++ app/src/main/java/com/shantanukamath/knockout/KnockOutApplication.java
@@ -1,5 +1,8 @@
 package com.shantanukamath.knockout;
+
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
@@ -13,5 +16,11 @@ public class KnockOutApplication extends Application {
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
         ParseFacebookUtils.initialize(this);
 
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
